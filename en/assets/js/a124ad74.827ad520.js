5c21b9f5e9cbd71fec8f0cd134574e48fb7c6835
docume2603 <docume2603@gmail.com>
2026-06-22 11:22:01 +0900

    Deploy website - based on 27a3878703e5b87e7ad8bf73ab949dccb13d5c12

--- en/assets/js/a124ad74.827ad520.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunktest_website=globalThis.webpackChunktest_website||[]).push([[46221],{66898:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/ds/en/blog/page/363","page":363,"postsPerPage":1,"totalPages":390,"totalCount":390,"previousPage":"/ds/en/blog/page/362","nextPage":"/ds/en/blog/page/364","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
