2794e74c7dbd0794b0a2a834c1291b49a21aa02c
docume2603 <docume2603@gmail.com>
2026-07-07 11:46:42 +0900

    Deploy website - based on c2758bdc975ab201cf78041d8ffbf5b1124d73ca

--- assets/js/cd94d2de.3d432605.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunktest_website=globalThis.webpackChunktest_website||[]).push([[64258],{79800:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/ds/blog/page/279","page":279,"postsPerPage":1,"totalPages":440,"totalCount":440,"previousPage":"/ds/blog/page/278","nextPage":"/ds/blog/page/280","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ en/assets/js/1af7f2f4.ef2db4ac.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunktest_website=globalThis.webpackChunktest_website||[]).push([[53746],{26824:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/ds/en/blog/page/140","page":140,"postsPerPage":1,"totalPages":444,"totalCount":444,"previousPage":"/ds/en/blog/page/139","nextPage":"/ds/en/blog/page/141","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
