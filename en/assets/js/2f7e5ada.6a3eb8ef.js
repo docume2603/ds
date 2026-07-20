10f89b702a985ec3e5806de732b21362143ea914
docume2603 <docume2603@gmail.com>
2026-07-20 11:48:23 +0900

    Deploy website - based on 1c81c5ff7de13405e0610b2e72b19d23526169b4

---
+++ en/assets/js/2f7e5ada.6a3eb8ef.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunktest_website=globalThis.webpackChunktest_website||[]).push([[12524],{61548:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/ds/en/blog/page/486","page":486,"postsPerPage":1,"totalPages":486,"totalCount":486,"previousPage":"/ds/en/blog/page/485","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
